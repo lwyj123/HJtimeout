bb98265d84c7d3c9ca8118f7561c7240175a1f48
lwyj123 <443474713@qq.com>
2017-04-25 14:21:20 +0800

    complete timeout part

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # HJtimeout
 This is a timeout and interval extension use es6, the reason why I develop this thing is that a bug in my program and I can't find what's wrong because I can't view all the timouts/intervals in javascript.
+
+# Install
+
+Waiting for deploy
+
+# Usage
+### HJtimeout.timer(callback, millisec)
+
+`callback` a function of your callback.
+
+`millisec` a time for setTimeout.
+
+### HJevent.cancelTimer(id)
+
+`id` a id of the Timer you wanna cancel.
+
+
+Example
+
+```js
+var timer = new HJtimeout();
+timer.timer(function() {
+    alert('timerfinished');
+}, 2000);
+```
--- src/HJtimeout.js
+++ src/HJtimeout.js
@@ -8,6 +8,8 @@ function set(that, key, value) {
     if(!attributions[that]) attributions[that] = {};
     attributions[that][key] = value
 }
+
+
 var eventEmitter = new HJevent();
 
 class HJtimeout {
@@ -20,25 +22,34 @@ class HJtimeout {
         //the number of Interval
         set(this, 'IVLen', 0);
         set(this, 'IVlist', []);
+        this.bindEvent();
+    }
+    bindEvent() {
+        let self = this;
+        eventEmitter.on('timberFinished', function(id) {
+            self.__removeTimeout(id);
+        });        
     }
 
     getTolen() {
         return get(this, 'TOLen');
+    }
+    getTOlist() {
+        return get(this, 'TOlist');
     }
     getIVLen() {
         return get(this, 'IVLen')
     }
     timer(callback, millisec) {
         set(this, 'TOLen', get(this, 'TOLen') + 1);
-        //封装一个新callback方便计算数和跟踪状态
-        let realCallback = function() {
-            return function() {
-                callback.apply(this, arguments);
-                eventEmitter.emit("timberFinished");
-                console.log("timberFinished");
-            }
-        }();
+
         let t = setTimeout(realCallback, millisec);
+        //封装一个新callback方便计算数和跟踪状态
+        function realCallback() {
+            callback.apply(this, arguments);
+            eventEmitter.emit("timberFinished", t);
+            console.log(`timberFinished id: ${t}`);
+        }
         get(this, 'TOlist').push({"id": t, "callback": callback.toString(), "millisec": millisec});
         return t;
     }
@@ -49,11 +60,17 @@ class HJtimeout {
         return t;
     }
     cancelTimer(id) {
-
-        clearTimeout(id);
-
+        this.__removeTimeout(id);
     }
     cancelTimerAll() {
+        let self = this;
+        //直接使用list会导致动态修改数组导致错误
+        let list = get(this, 'TOlist');
+        let clonelist = list.slice();
+
+        clonelist.map(function(obj) {
+            self.__removeTimeout(obj.id);
+        });
 
     }
     cancelInterval(id) {
@@ -62,6 +79,15 @@ class HJtimeout {
     cancelIntervalAll() {
 
     }
-
+    //移除Timeout，无论是完成还是取消
+    __removeTimeout(id) {
+        let list = get(this, 'TOlist');
+        let itemIndex = list.findIndex((obj) => obj.id == id);
+        if(itemIndex != -1) {
+            clearTimeout(list[itemIndex].id);
+            list.splice(itemIndex, 1);
+            set(this, 'TOLen', get(this, 'TOLen') - 1);
+        }
+    }
 }
 export default HJtimeout;
